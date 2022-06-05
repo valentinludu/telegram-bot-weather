2ed88edd8da603e81da76180801dd676c79345a3
valentinludu <ludu.valentin@yahoo.com>
2022-06-06 00:11:03 +0300

    feat: Removed /

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV !== 'production') {
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.telegram.setWebhook(
-  `${process.env.HEROKU_APP_URL}/bot${process.env.BOT_TOKEN}`
+  `${process.env.HEROKU_APP_URL}bot${process.env.BOT_TOKEN}`
 );
 bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, process.env.PORT);
 
