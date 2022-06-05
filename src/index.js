dae84cf07a36ac969aa2987284349fe038558271
valentinludu <ludu.valentin@yahoo.com>
2022-06-05 23:33:29 +0300

    feat: Added PORT and webhook for heroku

--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ if (process.env.NODE_ENV !== 'production') {
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+bot.telegram.setWebhook(
+  `${process.env.HEROKU_APP_URL}/bot${process.env.BOT_TOKEN}`
+);
+bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, process.env.PORT);
+
 bot.start((ctx) =>
   ctx.reply(
     'Welcome stranger. This is a bot specially build for you to see your current weather. \n\nType /help to see all the available actions. \n\nType a city name or click on a predefined city below:',
